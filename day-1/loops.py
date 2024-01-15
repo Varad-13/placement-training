cafea24f425ba5c0d6b8ef9e7ee141811c0080ac
Varad Joshi <varad.gcs@gmail.com>
2024-01-15 16:25:46 +0530

    Day 1: Probelms on loops

---
+++ day-1/loops.py
@@ -0,0 +1,23 @@
+x=10
+while(x>=1):
+    print(x)
+    x-=1
+
+for x in range(1, 12, 2):
+    print(x)
+
+for x in range(2, 13, 2):
+    print(x)
+
+x = int(input("Please enter a number (n>0): "))
+for x in range(1, x+1, 1):
+    print(x)
+
+for x in range(x, -1, -1):
+    print(x)
+
+for x in range(2, 6, 1):
+    print(x)
+
+for x in range(5, 1, 1):
+    print(x)
